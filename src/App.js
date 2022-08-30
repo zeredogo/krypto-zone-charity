f981ccc29cde0916d6282d63304f18b301b31459
zeredogo <zeredogo@gmail.com>
2022-08-30 23:26:20 +0100

    updating commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+
+const App = () => {
+  return (
+    <section>
+        <Navbar />
+        <Footer />
+    </section>
+  )
+}
+
+export default App
