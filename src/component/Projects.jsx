91c8422e0ce378f881585de7c8ab2a3899b2da0e
zeredogo <zeredogo@gmail.com>
2022-09-02 20:50:12 +0100

    updating commit

---
+++ src/component/Projects.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    
+       <section class="project py-5">
+        <div class="container">
+          <div class="row my-3">
+            <div class="col-10 mx-auto text-center">
+              <h1 class="text-uppercase">Our Latest Projects</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+                elit. Voluptates, rerum.</p>
+            </div>
+          </div>
+          
+          <div class="row">
+            
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/about-2.jpg" class="img-fluid grow" alt="project 1"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-1</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+           
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/project-2.jpeg" class="img-fluid grow" alt="project 2"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-2</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+           
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/project-3.jpeg" class="img-fluid grow" alt="project 3"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-3</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+           
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/project-6.jpg" class="img-fluid grow" alt="project 4"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-4</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+          
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/project-5.jpg" class="img-fluid grow" alt="project 5"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-5</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+           
+            <div class="col-10 col-md-6 col-lg-4">
+              <div class="project-container p-5 outline">
+                <img src="images/project-4.jpeg" class="img-fluid grow" alt="Project 6"/>
+                <a href="#" class="donation text-capitalize">Donate Now</a>
+              </div>
+              <h4 class="text-capitalize text-center my-2">Project-6</h4>
+              <h6 class="text-center">
+                Specification
+              </h6>
+            </div>
+         
+          </div>
+        </div>
+       </section>
+     
+  )
+}
+
+export default Projects
