898eca4c3b1b9da6da6ada19554b0ffeea2b0277
zeredogo <zeredogo@gmail.com>
2022-09-02 22:08:28 +0100

    updating commit

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,82 +1,88 @@
-import React from 'react'
+import React from 'react';
+import Project1 from '../images/about-2.jpg';
+import Project2 from "../images/project-2.jpeg";
+import Project3 from "../images/project-3.jpeg";
+import Project4 from "../images/project-6.jpg";
+import Project5 from "../images/project-5.jpg";
+import Project6 from "../images/project-4.jpeg";
 
 const Projects = () => {
   return (
     
-       <section class="project py-5">
-        <div class="container">
-          <div class="row my-3">
-            <div class="col-10 mx-auto text-center">
-              <h1 class="text-uppercase">Our Latest Projects</h1>
+       <section className="project py-5">
+        <div className="container">
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              <h1 className="text-uppercase">Our Latest Projects</h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Voluptates, rerum.</p>
             </div>
           </div>
           
-          <div class="row">
+          <div className="row">
             
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/about-2.jpg" class="img-fluid grow" alt="project 1"/>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project1} className="img-fluid grow" alt="project 1"/>
                 <a href="#" class="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-1</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-1</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
            
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/project-2.jpeg" class="img-fluid grow" alt="project 2"/>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project2} className="img-fluid grow" alt="project 2"/>
                 <a href="#" class="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-2</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-2</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
            
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/project-3.jpeg" class="img-fluid grow" alt="project 3"/>
-                <a href="#" class="donation text-capitalize">Donate Now</a>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project3} className="img-fluid grow" alt="project 3"/>
+                <a href="#" className="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-3</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-3</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
            
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/project-6.jpg" class="img-fluid grow" alt="project 4"/>
-                <a href="#" class="donation text-capitalize">Donate Now</a>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project4} className="img-fluid grow" alt="project 4"/>
+                <a href="#" className="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-4</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-4</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
           
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/project-5.jpg" class="img-fluid grow" alt="project 5"/>
-                <a href="#" class="donation text-capitalize">Donate Now</a>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project5} className="img-fluid grow" alt="project 5"/>
+                <a href="#" className="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-5</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-5</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
            
-            <div class="col-10 col-md-6 col-lg-4">
-              <div class="project-container p-5 outline">
-                <img src="images/project-4.jpeg" class="img-fluid grow" alt="Project 6"/>
-                <a href="#" class="donation text-capitalize">Donate Now</a>
+            <div className="col-10 col-md-6 col-lg-4">
+              <div className="project-container p-5 outline">
+                <img src={Project6} className="img-fluid grow" alt="Project 6"/>
+                <a href="#" className="donation text-capitalize">Donate Now</a>
               </div>
-              <h4 class="text-capitalize text-center my-2">Project-6</h4>
-              <h6 class="text-center">
+              <h4 className="text-capitalize text-center my-2">Project-6</h4>
+              <h6 className="text-center">
                 Specification
               </h6>
             </div>
