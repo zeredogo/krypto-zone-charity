1d23aeabe1773d7e5e8afbd6f9647d9d839aedc4
zeredogo <zeredogo@gmail.com>
2022-09-02 22:17:33 +0100

    updating commit

---
+++ src/component/Team.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+
+
+const Team = () => {
+    return (
+        
+       <section class="team">
+        <div class="container">
+          <div class="section-title">
+            <h2 class="text-center">Our Team</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Animi, aspernatur. Blanditiis velit facilis 
+              rerum laudantium at.</p>
+          </div>
+          <div class="row">
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="team-1">
+                <div class="pic">
+                  <img src="../images/image-1.jpg" class="img-fluid" alt=""/>
+                </div>
+                <div class="team-info">
+                  <h4>Wizkid</h4>
+                  <span>Specification</span>
+                  <div class="social">
+                    <a href="">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="team-1">
+                <div class="pic">
+                  <img src="../images/image-2.jpg" class="img-fluid" alt=""/>
+                </div>
+                <div class="team-info">
+                  <h4>Burna Boy</h4>
+                  <span>Specification</span>
+                  <div class="social">
+                    <a href="">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="team-1">
+                <div class="pic">
+                  <img src="../images/image-3.jpg" class="img-fluid" alt=""/>
+                </div>
+                <div class="team-info">
+                  <h4>Davido</h4>
+                  <span>Specification</span>
+                  <div class="social">
+                    <a href="">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="team-1">
+                <div class="pic">
+                  <img src="../images/image-3.jpg" class="img-fluid" alt=""/>
+                </div>
+                <div class="team-info">
+                  <h4>Ice Prince</h4>
+                  <span>Specification</span>
+                  <div class="social">
+                    <a href="">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i class="fab fa-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+       </section>
+    )
+}
+
+export default Team;
