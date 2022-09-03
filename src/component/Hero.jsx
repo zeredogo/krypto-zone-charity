71b2ad0f38d87ec124458dd52a656cbc6986def3
zeredogo <zeredogo@gmail.com>
2022-09-04 00:44:23 +0100

    updating commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -19,48 +19,48 @@ const Hero = () => {
               Aperiam iure beatae alias quod, ducimus explicabo.
               adipisicing elit.
             </p>
-            <a href="#" class="btn hero-btn animate__animated 
+            <a href="#" className="btn hero-btn animate__animated 
             animate__backInUp">
             Read More
           </a>
           </div>
         </div>
        
-         <div class="carousel-item ">
-          <div class="carousel-container">
-            <h2 class="animate__animated animate__backInDown">
+         <div className="carousel-item ">
+          <div className="carousel-container">
+            <h2 className="animate__animated animate__backInDown">
               Save A Life
             </h2>
-            <p class="animate__animated animate__fadeInUp">
+            <p className="animate__animated animate__fadeInUp">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Aperiam iure beatae alias quod, ducimus explicabo.
               adipisicing elit.
             </p>
-            <a href="#" class="btn hero-btn animate__animated 
+            <a href="#" className="btn hero-btn animate__animated 
             animate__backInUp">
             Read More
           </a>
           </div>
         </div>
       
-         <div class="carousel-item ">
-          <div class="carousel-container">
-            <h2 class="animate__animated animate__backInDown">
+         <div className="carousel-item ">
+          <div className="carousel-container">
+            <h2 className="animate__animated animate__backInDown">
               Make A Donation
             </h2>
-            <p class="animate__animated animate__fadeInUp">
+            <p className="animate__animated animate__fadeInUp">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Aperiam iure beatae alias quod, ducimus explicabo.
               adipisicing elit.
             </p>
-            <a href="#" class="btn hero-btn animate__animated 
+            <a href="#" className="btn hero-btn animate__animated 
             animate__backInUp">
             Read More
           </a>
           </div>
         </div>
         
-        <a class="carousel-control-prev" href="#heroCarousel" role="button"
+        <a className="carousel-control-prev" href="#heroCarousel" role="button"
         data-slide="prev">
         <span class="carousel-control-prev-icon fas
         fa-chevron-left" aria-hidden="true"></span>
