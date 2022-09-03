4085e0fbe8f730eaaebae810736496ad54ceacea
zeredogo <zeredogo@gmail.com>
2022-09-03 01:23:57 +0100

    updating commit

---
+++ src/component/FinishedProject.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+const FinishedProject = () => {
+  return (
+    
+       <section>
+        <div class="container">
+          <div class="text-center">
+            <h2 class="mb-5">Finished Projects</h2>
+          </div>
+          <div class="row no-gutters">
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-6.jpg" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-2.jpeg" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-3.jpg" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-4.jpg" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-5.jpg" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+            <div class="col-lg-6">
+              <a class="project-item" href="">
+                <span class="text">
+                  <span class="des">
+                    <h2>Lorem Ipsum</h2>
+                    <p class="mb-0">
+                      Lorem ipsum dolor sit, amet consectetur 
+                      adipisicing elit. Molestiae, sunt.
+                    </p>
+                  </span>
+                </span>
+                <img src="images/finished-7.webp" class="img-fluid" alt="">
+              </a>
+            </div>
+            
+          </div>
+        </div>
+       </section>
+       
+  )
+}
+
+export default FinishedProject
