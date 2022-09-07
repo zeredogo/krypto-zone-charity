1b538ce1839cf58ca0dbceede03e5416d0e5ce31
zeredogo <zeredogo@gmail.com>
2022-09-08 00:29:12 +0100

    updating commit

--- src/component/FinishedProject.jsx
+++ src/component/FinishedProject.jsx
@@ -21,7 +21,7 @@ const FinishedProject = () => {
                   <span className="des">
                     <h2>Lorem Ipsum</h2>
                     <p className="mb-0">
-                      Lorem ipsum dolor sit, amet consectetur 
+                      Lorem ipsum dolor sit, Emit ifakus amet consectetur 
                       adipisicing elit. Molestiae, sunt.
                     </p>
                   </span>
