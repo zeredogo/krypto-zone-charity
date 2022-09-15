3df285409c622f08a09f9df4512f36638dd3e48d
zeredogo <zeredogo@gmail.com>
2022-09-16 00:10:39 +0100

    changes made

--- src/component/FinishedProject.jsx
+++ src/component/FinishedProject.jsx
@@ -39,7 +39,8 @@ const FinishedProject = () => {
                     <p className="mb-0">
                       This project was done in Jos to 
                       Educate more than 1 million Studen 
-                      for free and we will help secure the world.  
+                      for free and we will help secure the world
+                      from the extention of.  
                     </p>
                   </span>
                 </span>
