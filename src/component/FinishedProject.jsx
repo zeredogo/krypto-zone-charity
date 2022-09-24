37a8ec83d8dabe8663a222bd27ee78d941092522
zeredogo <zeredogo@gmail.com>
2022-09-25 00:12:13 +0100

    updating commit

--- src/component/FinishedProject.jsx
+++ src/component/FinishedProject.jsx
@@ -70,9 +70,9 @@ const FinishedProject = () => {
                   <span className="des">
                     <h2>Clean Water</h2>
                     <p className="mb-0">
-                      This project was done in over 30 local 
+                      This project was done in over 300 local 
                       government to produces clean water for 
-                      the community. exacle saff xemas dislap xall sap as ged xat javamy , dfma
+                      the community. exacles as saff xemas dislap xall sap as ged xat javamy , dfma
                       as xina jose'a blue jamanu  hull vad skull governmente  hub n jc sh i 
                       ebhve bebbiuchn erfh  nav, school  massive reward.
                     </p>
